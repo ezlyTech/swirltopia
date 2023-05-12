1ac240c98477568fb3d54920850522c284387d66
Puffssss1 <108129228+Puffssss1@users.noreply.github.com>
2023-05-12 15:36:32 +0800

    for trial

--- src/js/partials/fullbanner.js
+++ src/js/partials/fullbanner.js
@@ -1,12 +1,14 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     var splides = document.querySelectorAll('.splide');
     for (var i = 0; i < splides.length; i++) {
       var options = {};
       if (splides[i].classList.contains('herobanner-slider')) {
+        console.log('test');
         options.arrows = false;
         options.type = 'fade';
 	      options.autoplay = true;
-        options.interval = 3000;
+        options.interval = 1000;
         options.wheel = false;
         options.rewind = true;
         options.pauseOnHover = false;
